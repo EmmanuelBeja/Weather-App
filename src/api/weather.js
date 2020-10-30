94ce2845538994028f3b51c5f5ebd59d47239890
EmmanuelChayu <emmanuel.chayu@incentro.com>
2020-10-30 11:05:32 +0300

    configuree gh-pages deployment

--- src/api/weather.js
+++ src/api/weather.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 
 const URL = 'https://api.openweathermap.org/data/2.5/weather';
-const API_KEY = process.env.REACT_APP_OPEN_WEATHER_API_KEY;
+// const API_KEY = process.env.REACT_APP_OPEN_WEATHER_API_KEY;
+const API_KEY = `f33a484cf794d08d0148764789aaba32`
 
 export const fetchWeather = location => axios.get(URL, {
     params: {
