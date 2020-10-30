3e40811443890409ee4131f43e2975bc28db026f
EmmanuelChayu <emmanuel.chayu@incentro.com>
2020-10-30 11:11:41 +0300

    Updates

---
+++ precache-manifest.4c99b52f86c3704fbac6937d3273ad0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "369046b7eeed5aaf47026bcd5f785f64",
+    "url": "/Weather-App/index.html"
+  },
+  {
+    "revision": "0a88b54f851273163a3e",
+    "url": "/Weather-App/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "2f87f4219a2409d7638d",
+    "url": "/Weather-App/static/css/main.3c844e7b.chunk.css"
+  },
+  {
+    "revision": "0a88b54f851273163a3e",
+    "url": "/Weather-App/static/js/2.52250ac1.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Weather-App/static/js/2.52250ac1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f87f4219a2409d7638d",
+    "url": "/Weather-App/static/js/main.78397d07.chunk.js"
+  },
+  {
+    "revision": "b691b53e305b9dcdfed5",
+    "url": "/Weather-App/static/js/runtime-main.a97a20e2.js"
+  }
+]);
